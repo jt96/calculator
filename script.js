91081c0efffd3d87243894f97ac24b6d1bc5a0b4
Jackie Trenh <29517154+jt96@users.noreply.github.com>
2024-07-08 22:57:21 -0700

    add calculator functionality and styling

--- script.js
+++ script.js
@@ -1,31 +1,161 @@
-const add = function(a, b) {
-    return a + b;
-};
+    const add = function(a, b) {
+        return parseFloat(a) + parseFloat(b);
+    };
 
-const subtract = function(a, b) {
-    return a - b;
-};
+    const subtract = function(a, b) {
+        return parseFloat(a) - parseFloat(b);
+    };
 
-const sum = function(numArray) {
-    return numArray.reduce((total, currentNum) => {
-        return total + currentNum;
-    }, 0);
-};
+    const multiply = function(a, b) {
+        return parseFloat(a) * parseFloat(b);
+    };
 
-const multiply = function(numArray) {
-    return numArray.reduce((total, currentNum) => {
-        return total * currentNum;
-    }, 1);
-};
+    const divide = function(a, b) {
+        return parseFloat(a) / parseFloat(b);
+    }
 
-const power = function(base, exponent) {
-    return base ** exponent;
-};
+    function operate(operator, firstNum, secNum) {
+        let result;
+        switch (operator) {
+            case "+":
+                result = add(firstNum, secNum);
+                break;
+            case "-":
+                result = subtract(firstNum, secNum);
+                break;
+            case "*":
+                result = multiply(firstNum, secNum);
+                break;
+            case "/":
+                if (secNum == 0) {
+                    alert("You have broken the calculator!");
+                    clearCalculator();
+                    result = 0;
+                    break;
+                }
+                result = divide(firstNum, secNum);
+                break;
+        }
+        return result;
+    }
 
-module.exports = {
-    add,
-    subtract,
-    sum,
-    multiply,
-    power
-};
+    function display(num) {
+        let displayField = document.querySelector(".display");
+        displayField.textContent = num.toString();
+    }
+
+    function evaluate() {
+        console.log([operator, firstNum, secNum]);
+        let result = operate(operator, firstNum, secNum);
+        firstNum = result;
+        secNum = null;
+        display(result);
+    }
+
+    function clearCalculator() {
+        display(0);
+        firstNum = null;
+        secNum = null;
+        operator = null;
+        nextNum = false;
+    }
+
+    let firstNum;
+    let secNum;
+    let operator;
+    let nextNum = false;
+
+    const numbers = document.querySelectorAll(".number");
+    numbers.forEach(number => {
+        number.addEventListener("click", function() {
+            if (firstNum == null) {
+                firstNum = number.textContent;
+            } else if (firstNum != null && nextNum == false) {
+                firstNum += number.textContent;
+            } else if (secNum == null) {
+                secNum = number.textContent;
+            } else {
+                secNum += number.textContent;
+            }
+            if (secNum == null) {
+                display(firstNum);
+            } else {
+                display(secNum);
+            }
+            console.log([firstNum, secNum]);
+        });
+    });
+
+    const decimal = document.querySelector(".decimal");
+    decimal.addEventListener("click", function() {
+        if (firstNum != null && secNum == null && !firstNum.includes(".")) {
+            firstNum += ".";
+        } else if (secNum != null && !secNum.includes(".")) {
+            secNum += ".";
+        }
+        if (secNum == null) {
+            display(firstNum);
+        } else {
+            display(secNum);
+        }
+    });
+
+    const addBtn = document.querySelector(".add");
+    addBtn.addEventListener("click", function() {
+        if (firstNum != null  && secNum != null && operator != null) {
+            evaluate();
+        }
+        operator = addBtn.textContent;
+        nextNum = true;
+    });
+
+    const subtractBtn = document.querySelector(".subtract");
+    subtractBtn.addEventListener("click", function() {
+        if (firstNum != null  && secNum != null && operator != null) {
+            evaluate();
+        }
+        operator = subtractBtn.textContent;
+        nextNum = true;
+    });
+
+    const multiplyBtn = document.querySelector(".multiply");
+    multiplyBtn.addEventListener("click", function() {
+        if (firstNum != null  && secNum != null && operator != null) {
+            evaluate();
+        }
+        operator = "*";
+        nextNum = true;
+    });
+
+    const divideBtn = document.querySelector(".divide");
+    divideBtn.addEventListener("click", function() {
+        if (firstNum != null  && secNum != null && operator != null) {
+            evaluate();
+        }
+        operator = "/";
+        nextNum = true;
+    });
+
+    const clear = document.querySelector(".clear");
+    clear.addEventListener("click", function() {
+        clearCalculator();
+    });
+
+    const equals = document.querySelector(".evaluate");
+    equals.addEventListener("click", function() {
+        if (operator != null && secNum != null) {
+            evaluate();
+            operator = null;
+            nextNum = false;
+        }
+    })
+
+    /*
+    module.exports = {
+        add,
+        subtract,
+        multiply,
+        divide,
+        power
+    };
+    */
